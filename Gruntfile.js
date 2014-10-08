b9cb1de315232c7cfb8415481b15dfb50df292bb
Jonathan L. Meek <lemuelmeek@gmail.com>
2014-10-08 15:09:18 -0400

    Updated GruntFile to use the karma config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,18 @@
 module.exports = function(grunt) {
   'use strict';
   grunt.initConfig({
-    jasmine : {
-	// Your project's source files
-	src : [ 'bower_components/angular/angular.js',
-		'bower_components/angular-mocks/angular-mocks.js',
-		'bower_components/angular-strap/dist/angular-strap.js',
-		'bower_components/jquery/dist/jquery.min.js',
-		'bower_components/bootbox/bootbox.js',
-		'bower_components/bootstrap/dist/js/bootstrap.min.js',
-		'vendor/angular-route.js',
-		'vendor/angular-resource.js',
-		'vendor/ng-table.js',
-		'app/intro.js',
-		'app/controllers/*.js'],
-	// Your Jasmine spec files
-	options : {
-	  specs : 'testUnit.js'
+    karma:{
+	unit:{
+	    configFile: 'karma.conf.js'
 	}
-    }//end jasmine
-    
+    }
   });
 
   // Register tasks.
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-karma');
 
   // Default task.
-  grunt.registerTask('default', 'jasmine');
+  grunt.registerTask('default', 'karma');
   
 };
 
